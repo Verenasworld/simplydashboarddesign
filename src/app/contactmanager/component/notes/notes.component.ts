1dfb2749ee07b65b436faf199d3864e6bcfc0d30
Verenasworld <verena.felizidas@gmail.com>
2021-02-25 15:51:41 +0100

    adding Header Sorting

--- src/app/contactmanager/component/notes/notes.component.ts
+++ src/app/contactmanager/component/notes/notes.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core'
 import {Note} from '../../models/note';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-notes',
@@ -17,9 +18,12 @@ export class NotesComponent implements OnInit, AfterViewInit {
   constructor() { }
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
   ngOnInit(): void {
     this.dataSource = new MatTableDataSource<Note>(this.notes);
