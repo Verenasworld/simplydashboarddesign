8cf5e7802168ec2ab3259d5581c50d64ddb433a2
Verenasworld <verena.felizidas@gmail.com>
2021-02-25 15:45:46 +0100

    adding Filtering

--- src/app/contactmanager/component/notes/notes.component.ts
+++ src/app/contactmanager/component/notes/notes.component.ts
@@ -1,13 +1,14 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
 import {Note} from '../../models/note';
 import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.scss']
 })
-export class NotesComponent implements OnInit {
+export class NotesComponent implements OnInit, AfterViewInit {
   @Input() notes: Note[];
 
   displayedColumns: string[] = ['position', 'title', 'date'];
@@ -15,8 +16,16 @@ export class NotesComponent implements OnInit {
 
   constructor() { }
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
   ngOnInit(): void {
     this.dataSource = new MatTableDataSource<Note>(this.notes);
   }
-
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
